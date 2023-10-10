3df38d332eda414f11de837f28b9300164ccb15a
Nimma Soumya <118757238+soumyachikki@users.noreply.github.com>
2023-10-10 21:33:30 +0530

    Add files via upload

---
+++ Extension Code.py
@@ -0,0 +1,8 @@
+# Input the filename
+filename = input("Input the Filename: ")
+
+# Split the filename into its base and extension
+base, extension = filename.split(".")
+
+# Display the extension
+print(f"The extension of the file is: '{extension}'")
